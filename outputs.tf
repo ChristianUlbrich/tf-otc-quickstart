db216e0c3d818f8dc38b64c868b52aabae26436a
Christian Ulbrich <christian.ulbrich@zalari.de>
2026-01-14 00:40:59 +0100

    docs: make outputs more meaningful and more HCLy

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,7 @@
-output "external-ip" {
-  value = ["ssh ubuntu@${opentelekomcloud_networking_floatingip_v2.eip_quickstart.address}"]
+output "external_ip" {
+  value = opentelekomcloud_networking_floatingip_v2.eip_quickstart.address
 }
 
+output "ssh_command" {
+  value = "ssh ubuntu@${opentelekomcloud_networking_floatingip_v2.eip_quickstart.address}"
+}

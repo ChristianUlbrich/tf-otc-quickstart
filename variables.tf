9c5d3629d385ad5823a85529450f88c395a8c7c7
Christian Ulbrich <christian.ulbrich@zalari.de>
2026-01-14 00:25:25 +0100

    feat: add types to variables

--- variables.tf
+++ variables.tf
@@ -1,79 +1,96 @@
 # OTC vars
 variable "access_key" {
+  type        = string
   description = "Auth Access Key"
+  sensitive   = true
 }
 
 variable "secret_key" {
+  type        = string
   description = "Auth Secret Key"
+  sensitive   = true
 }
 
 variable "auth_url" {
-  description = "IAM Auth Url"
+  type        = string
   default     = "https://iam.eu-de.otc.t-systems.com:443/v3"
+  description = "IAM Auth Url"
 }
 
 variable "tenant_name" {
-  description = "Name of the tenant"
+  type        = string
   default     = "eu-de"
+  description = "Name of the tenant"
 }
 
 variable "region" {
-  description = "Cloud Region"
+  type        = string
   default     = "eu-de"
+  description = "Cloud Region"
 }
 
 variable "availability_zone" {
-  description = "Availability Zone"
+  type        = string
   default     = "eu-de-01"
+  description = "Availability Zone"
 }
 
 variable "vpc_name" {
-  description = "Name of VPC"
+  type        = string
   default     = "quickstart-vpc"
+  description = "Name of VPC"
 }
 
 variable "vpc_cidr" {
-  description = "VPC network"
+  type        = string
   default     = "192.168.0.0/16"
+  description = "VPC network"
 }
 
 variable "subnet_name" {
-  description = "Subnetwork name"
+  type        = string
   default     = "quickstart-subnet"
+  description = "Subnetwork name"
 }
 
 variable "subnet_cidr" {
-  description = "Sub Network CIDR"
+  type        = string
   default     = "192.168.1.0/24"
+  description = "Sub Network CIDR"
 }
 
 variable "dns_list" {
-  description = "list of DNS servers"
+  type        = list(string)
   default     = ["100.125.4.25", "8.8.8.8"]
+  description = "list of DNS servers"
 }
 
 variable "subnet_gateway_ip" {
-  description = "Subnet Gateway"
+  type        = string
   default     = "192.168.1.1"
+  description = "Subnet Gateway"
 }
 
 variable "secgroup_name" {
-  description = "Secgroup name"
+  type        = string
   default     = "quickstart-secgroup"
+  description = "Secgroup name"
 }
 
 variable "image_name" {
   description = "Name of the image"
   default     = "Standard_Ubuntu_18.04_latest"
+  type        = string
 }
 
 variable "flavor_id" {
   description = "ID of Flavor"
   default     = "c3.large.2"
+  type        = string
 }
 
 variable "public_key" {
   description = "ssh public key to use"
+  type        = string
   default     = ""
 }
-
